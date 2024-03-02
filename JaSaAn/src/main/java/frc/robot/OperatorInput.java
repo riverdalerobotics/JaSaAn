ab80f0b3b924bbd74e7a9f4c35ed7de6f01d0010
Sakk1na <jasminekonare@gmail.com>
2024-03-02 12:06:29 -0500

    test commit

--- JaSaAn/src/main/java/frc/robot/OperatorInput.java
+++ JaSaAn/src/main/java/frc/robot/OperatorInput.java
@@ -2,6 +2,7 @@ public class OperatorInput {
     XboxController moveController;  
     XboxController intakeController;
     
+    
     public OperatorInput() {
         moveController = new XboxController(0);
         intakeController = new XboxController(1);
