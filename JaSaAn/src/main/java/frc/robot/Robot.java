c2a097927d95089f38b27efe0b5586fc578604f8
Sana-Shahd <sanaperzeen@gmail.com>
2024-03-19 14:15:11 -0700

    added intake

--- JaSaAn/src/main/java/frc/robot/Robot.java
+++ JaSaAn/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.DefaultCommands.IntakeCommand;
+import frc.robot.subsystems.IntakeSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,7 +19,10 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+  public static final IntakeSubsystem INTAKE_SUBSYSTEM= new IntakeSubsystem();
+  public static final OI oi =  new OI();
+//  private RobotContainer m_robotContainer;
+  public static final IntakeCommand INTAKE_COMMAND = new IntakeCommand();
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -27,7 +32,8 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+//    m_robotContainer = new RobotContainer();
+    CommandScheduler.getInstance().setDefaultCommand(INTAKE_SUBSYSTEM, INTAKE_COMMAND);
   }
 
   /**
@@ -56,7 +62,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+  //  m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
@@ -81,7 +87,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
   @Override
   public void testInit() {
--- JaSaAn/src/main/java/frc/robot/commands/DefaultCommands/IntakeCommand.java
+++ JaSaAn/src/main/java/frc/robot/commands/DefaultCommands/IntakeCommand.java
@@ -2,25 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.DefaultCommands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.Robot;
+import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class IntakeCommand extends Command {
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+
+  public IntakeCommand() {
+  
+
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(Robot.INTAKE_SUBSYSTEM);
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +25,14 @@ public class ExampleCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if(Robot.oi.intake()){
+      Robot.INTAKE_SUBSYSTEM.turnONIntake(-0.4);
+    }
+    else if (Robot.oi.intake() == false){
+      Robot.INTAKE_SUBSYSTEM.turnONIntake(0);
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
