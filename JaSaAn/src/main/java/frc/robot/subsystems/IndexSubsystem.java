8ce071d3999ecf7c838f0f85c3f8815e5c4f125c
Angusbutinriverdale <anguszheng649@gmail.com>
2024-03-19 17:40:08 -0400

    started writing index

---
+++ JaSaAn/src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+public class IndexSubsystem extends SubsystemBase {
+  
+  WPI_VictorSPX indexMotor;
+
+
+  
+  public IndexSubsystem() {
+    indexMotor = new WPI_VictorSPX(1000);
+  }
+
+
+  
+  public void indexBackwards() {
+
+    indexMotor.set(-1);
+    
+  }
+  
+  public void indexForwards() {
+
+    indexMotor.set(1);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+
+}
