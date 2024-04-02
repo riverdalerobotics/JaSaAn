b99d0547c3904bcb73e4f6dea3710e66bb372bfb
Angusbutinriverdale <anguszheng649@gmail.com>
2024-04-02 16:03:46 -0400

    Index Changes (Angus)(Added comments)

--- JaSaAn/src/main/java/frc/robot/OI.java
+++ JaSaAn/src/main/java/frc/robot/OI.java
@@ -5,18 +5,30 @@ import edu.wpi.first.wpilibj.XboxController;
 public class OI {
     XboxController operatorController;
     
+    
     public OI() {
         operatorController = new XboxController(0);
     }
+    /**
+     * A button to lift the intake
+     * @return True if the left bumper is pressed
+     */
     public boolean intake(){
         return operatorController.getLeftBumper();
     }
-
+    /**
+     * 
+     * @return true if the A button is pressed
+     */
     public boolean indexForwards() {
-        return operatorController.getAButtonPressed();
+        return operatorController.getAButton();
     }
+    /**
+     * 
+     * @return true if the B button is pressed
+     */
     public boolean indexBackwards() {
-        return operatorController.getBButtonPressed();
+        return operatorController.getBButton();
     }
  //   public double 
 
